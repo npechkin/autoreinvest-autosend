c125988bb08448cef0e75d4cfef903766cabdf4d
npechkin <52535105+npechkin@users.noreply.github.com>
2019-07-30 20:19:12 +0300

    Add files via upload

---
+++ mhctools/my_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+############################# SEND EXAMPLE #####################################
+net=main
+address=0x00_YOUR_ADDRESS
+to=0x00_ADDRESS_FOR_SEND
+round_to_integer=0
+#./send_all.sh  $net $address $to $round_to_integer
+########################### REINVEST EXAMPLE ###################################
+net=main
+address=0x00_YOUR_ADDRESS
+node=0x00ADDRESS_OF_NODES
+round_to_integer=0
+#./reinvest $net $address $node $round_to_integer
+################################################################################
+cd /root/mhc/mhctools
+############################# SEND1 ############################################
+net=main
+address=
+to=
+round_to_integer=0
+#./send_all.sh  $net $address $to $round_to_integer
+############################# SEND2 ############################################
+net=main
+address=
+to=
+round_to_integer=0
+#./send_all.sh  $net $address $to $round_to_integer
+############################ REINVEST1 #########################################
+net=main
+address=
+node=
+round_to_integer=0
+#./reinvest.sh $net $address $node $round_to_integer
+############################ REINVEST2 #########################################
+net=main
+address=
+node=
+round_to_integer=0
+#./reinvest.sh $net $address $node $round_to_integer
+################################################################################

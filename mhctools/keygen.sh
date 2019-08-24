dc75c4521f3c95a8ef76309f36b0283749011165
npechkin <52535105+npechkin@users.noreply.github.com>
2019-08-24 20:34:36 +0300

    Add files via upload

--- mhctools/keygen.sh
+++ mhctools/keygen.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 #############   CONFIG   ###########################
-string="0x001"
+begin="0x008"
+string="0"
+end="88"
 n=1000000000000
 ####################################################
+save-keys () {
+    mv $address.pem keys
+    mv $address.pub keys
+}
+del-keys () {
+    rm $address.pem
+    rm $address.pub
+}
 i=0
 while [ "$i" -lt "$n" ]; do
     address=`./metahash.sh generate | grep 'Your metahash address is' | awk -F' ' '{print $5}'`
-    add=`echo $address | grep $string`
-    if [ "$add" == "$address" ]; then
-	echo address=$address
+    s=`echo $address | grep $begin | grep $string`
+    if [ "$s" == "$address" ]; then
+	se=$s'_END'
+	e=`echo $se | grep $end'_END'`
+	if [ "$e" == "$se" ]; then
+	    save-keys
+	    echo address=$address
+	else
+	    del-keys
+	fi
     else
-	rm $address.pem
-	rm $address.pub
+	del-keys
     fi
     i=$(($i+1))
 done
